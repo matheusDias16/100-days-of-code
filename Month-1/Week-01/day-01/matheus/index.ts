26c219eb3b1eeb98958f6bcc48badf6174853706
matheus <matzeraa16@gmai.com>
2024-05-29 19:34:15 -0300

    dia-1

---
+++ Month-1/Week-01/day-01/matheus/index.ts
@@ -0,0 +1,8 @@
+console.log("Coloque aqui sua idade:"); let idade = Number(18); console.log(idade + " anos");
+let ano = 365
+
+function calcular(){
+    return idade * ano
+ 
+}
+ console.log("A sua idade em dias é:",calcular());
